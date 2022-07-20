e5834d6be92924998ec5ce8b15f793c7f6c58be8
grounded-tako <sweetoctopus@outlook.com>
2022-07-20 15:35:10 -0700

    initial commit, client and server side setup

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResonse: "" };
+  }
+
+  testRoute() {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.state({ apiResonse: res }))
+      .catch((err) => err);
+  }
+
+  componentDidMount() {
+    this.testRoute();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to The Best Game Release Website</h1>
+        </header>
+        <p className="App-intro">{this.state.apiResonse}</p>
+      </div>
+    )
+  }
+}
+
+export default App;
