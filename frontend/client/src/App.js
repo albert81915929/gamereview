a8d535e9f00977cdee52d1bb2ad93caa144cc0b9
albert81915929 <albert81915929@gmail.com>
2022-07-23 16:51:08 -0400

    ver 0.0.2 demo

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -2,34 +2,60 @@ import logo from "./nintendo-2.svg";
 import "./App.css";
 import React, { Component } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResonse: "" };
-  }
+import GameCard from "./components/card";
+import ButtonDemo from "./components/ButtonDemo";
+import Appbar from "./components/Appbar";
+import { Container } from "@mui/system";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/nintendoStyles";
 
-  testRoute() {
-    fetch("http://localhost:9000/testAPI")
-      .then((res) => res.text())
-      .then((res) => this.state({ apiResonse: res }))
-      .catch((err) => err);
-  }
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { apiResonse: "" };
+//   }
 
-  componentDidMount() {
-    this.testRoute();
-  }
+//   testRoute() {
+//     fetch("http://localhost:9000/testAPI")
+//       .then((res) => res.text())
+//       .then((res) => this.state({ apiResonse: res }))
+//       .catch((err) => err);
+//   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to The Best Game Release Website</h1>
-        </header>
-        <p className="App-intro">{this.state.apiResonse}</p>
+//   componentDidMount() {
+//     this.testRoute();
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>GameTracker</h1>
+//         <img src={logo} className="App-logo" alt="logo" />
+        
+//       </div>
+//     )
+//   }
+// }
+export default function App(){
+  return (
+    <ThemeProvider theme = {theme}>
+      <div className="App" >
+        <Appbar/>
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <ButtonDemo/> */}
+        <Container sx = {{
+          display: 'block',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: '10px',
+          width: '20%'
+        }}>
+          <GameCard/>
+        </Container>
+        
       </div>
-    )
-  }
+    </ThemeProvider>
+  );
 }
 
-export default App;
+
